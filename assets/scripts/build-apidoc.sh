bb0d67dfea3cfa55d4c287f158569fc93c369d19
David Delassus <david.jose.delassus@gmail.com>
2021-07-14 16:42:43 +0200

    :bug: add submodules to fetch graphql schemas

--- assets/scripts/build-apidoc.sh
+++ assets/scripts/build-apidoc.sh
@@ -2,14 +2,10 @@
 
 set -e
 
-# Clone Git repositories
-git clone https://github.com/datapio/sdk.git ./sdk
-git clone git@github.com:link-society/kubirds-operator.git ./operator
-
 # Generate complete schema
 touch ./assets/schema.graphql
-cat sdk/sources/node/k8s-operator/src/graphql/type-defs/*.graphql > ./assets/schema.graphql
-cat operator/sources/controller/src/api/type-defs/*.graphql >> ./assets/schema.graphql
+cat ./assets/modules/sdk/sources/node/k8s-operator/src/graphql/type-defs/*.graphql > ./assets/schema.graphql
+cat ./assets/modules/operator/sources/controller/src/api/type-defs/*.graphql >> ./assets/schema.graphql
 
 # Build API documentation from schema
 npx spectaql ./assets/spectaql.yml -t static/api-doc
